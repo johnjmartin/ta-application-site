ad02b02d70bbbb9e705fd1171755b7bef6dfa29d
Eric Braun <Eric Braun>
2017-01-16 22:12:03 -0500

    Removed external links from user schema, updated signup view

--- app/models/user.js
+++ app/models/user.js
@@ -5,30 +5,12 @@ var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our user model
 var userSchema = mongoose.Schema({
-
     local            : {
+        name         : String,
+        lname        : String,
         email        : String,
         password     : String,
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
     }
-
 });
 
 // methods ======================
